f156d2571714750671f201a78fc0afbc66c0ca16
SANGL <sangleakash990@gmail.com>
2024-10-11 21:07:42 +0530

    welcom_api

---
+++ src/test/java/com/example/well/WelcomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.well;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
